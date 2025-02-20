5887ae5a3063cc78e8faca3f296594513fe47baa
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-20 23:48:38 +0300

    increase the login limit to 1min

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const { logEvents } = require('./logger');
 
 const loginLimiter = rateLimit({
-    windowMs: 30 * 1000, // 1 minute
+    windowMs: 60 * 1000, // 1 minute
     max: 5, // Limit each IP to 5 login requests per `window` per minute
     message: {
         message:
